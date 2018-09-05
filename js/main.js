061d8421daa714d5c159bc769ba8ad117c19a34b
RookieYang <yy335281965@gmail.com>
2018-09-05 23:05:01 +0800

    美化按钮

---
+++ js/main.js
@@ -0,0 +1,10 @@
+b.onclick=function(){
+  b.querySelector('.circle').classList.add('active')
+}
+
+b.querySelector('.circle')
+.addEventListener('transitionend'
+,function(){
+  b.querySelector('.circle')
+  .classList.remove('active')
+})
